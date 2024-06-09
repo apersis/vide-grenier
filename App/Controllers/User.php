a2a3cf0e4bf56b6c308229ac4b2b9c64b8bf9d7d
Eloïc Lesellier <119588462+apersis@users.noreply.github.com>
2024-06-09 19:18:37 +0200

    pas de doublons email

--- App/Controllers/User.php
+++ App/Controllers/User.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Config;
 use App\Model\UserRegister;
 use App\Models\Articles;
+use App\Models\User as ModelsUser;
 use App\Utility\Hash;
 use App\Utility\Session;
 use \Core\View;
@@ -44,13 +45,17 @@ class User extends \Core\Controller
     {
         if(isset($_POST['submit'])){
             $f = $_POST;
+            
+            $nbrSameEmail = ModelsUser::getByLogin($f['email']);
 
-            if($f['password'] == $f['password-check']){
+            if($nbrSameEmail == 0){
+                if($f['password'] == $f['password-check']){
 
-                $this->register($f);
-                $this->login($f);
-
-            }
+                    $this->register($f);
+                    $this->login($f);
+    
+                }
+            }           
             
             header('Location: /account');
         }
